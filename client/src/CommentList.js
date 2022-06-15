704b7b016d63fe2fa391a24a0c5d83060241d42f
Ahmed Mattar <AceMattar@gmail.com>
2022-06-15 06:21:18 +0200

    feat:client: using the query service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,23 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import axios from "axios";
-
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([{}]);
-
-  const fetchComments = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchComments();
-    // eslint-disable-next-line
-  }, []);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
